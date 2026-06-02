8c6457aae7966366d256b11368faf8af74008ed7
maxiheissi <mhe@aon.at>
2026-06-02 23:22:10 +0200

    added coloring of border and errors

--- src/ui.rs
+++ src/ui.rs
@@ -1,19 +1,29 @@
 use crate::app::{App, OutputBase};
 use ratatui::layout::{Alignment, Constraint, Direction, Layout};
+use ratatui::style::{Color, Style, Stylize};
 use ratatui::{
     Frame,
     layout::Rect,
     widgets::{Block, Borders, List, ListItem, Paragraph},
 };
 
+fn make_item(text: String, is_error: bool) -> ListItem<'static>
+{
+    if is_error
+    {
+        ListItem::new(text).style(Style::default().fg(Color::Red))
+    }
+    else
+    {
+        ListItem::new(text)
+    }
+}
+
 pub fn draw(frame: &mut Frame, app: &App)
 {
     let chunks = Layout::default()
         .direction(Direction::Vertical)
-        .constraints([
-            Constraint::Length(1), // Statusleiste
-            Constraint::Min(1),    // Liste
-        ])
+        .constraints([Constraint::Length(1), Constraint::Min(1)])
         .split(frame.area());
 
     let mut items: Vec<ListItem> = Vec::new();
@@ -31,7 +41,21 @@ pub fn draw(frame: &mut Frame, app: &App)
             },
             Err(e) => format!("{:?}", e),
         };
-        items.push(ListItem::new(format!("{} = {}", e.input, result_str)));
+
+        items.push(make_item(
+            format!("{} = {}", e.input, result_str),
+            e.result.is_err(),
+        ));
+
+        // let item = match &e.result
+        // {
+        //     Ok(_) => ListItem::new(format!("{} = {}", e.input, result_str)),
+        //     Err(_) => ListItem::new(format!("{} = {}", e.input, result_str))
+        //         .style(Style::default().fg(Color::Red)),
+        // };
+        // items.push(item);
+
+        // items.push(ListItem::new(format!("{} = {}", e.input, result_str)));
     }
 
     items.push(ListItem::new(app.input.as_str()));
@@ -45,10 +69,18 @@ pub fn draw(frame: &mut Frame, app: &App)
             Ok(n) => format!("{}", n),
             Err(e) => format!("{:?}", e),
         };
-        items.push(ListItem::new(format!("{} = {}", e.input, result_str)));
+
+        items.push(make_item(
+            format!("{} = {}", e.input, result_str),
+            e.result.is_err(),
+        ));
     }
 
-    let list = List::new(items).block(Block::default().borders(Borders::ALL));
+    let list = List::new(items).block(
+        Block::default()
+            .borders(Borders::ALL)
+            .border_style(Style::default().fg(Color::Rgb(122, 121, 28))),
+    );
 
     frame.render_widget(list, chunks[1]);
     frame.set_cursor_position(ratatui::layout::Position {
